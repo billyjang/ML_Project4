88d1b679f775c0f3be9105dfc357a6216ad85865
unknown <eyliu20@amherst.edu>
2018-12-11 18:02:57 -0500

    finished friend code

--- Competition.py
+++ Competition.py
@@ -28,14 +28,12 @@ def binary_search(a, x, lo=0, hi=None):  # can't use a to specify default for hi
 
 def generate_average_location(train_data, test_data, graph, flat_graph):
     to_return = []
-    # pointer = 0
-
     edge_pointer = 0
     flat_train = train_data[:, 0].flatten()
     # print("flat_train: ", flat_train)
-
+    # isequal = train_data == test_data
     for datum in test_data:
-        print("Currently on data id: ", datum[0])
+        # print("Currently on data id: ", datum[0])
         sum_latitude = 0
         sum_longitude = 0
         n = 0
@@ -49,10 +47,13 @@ def generate_average_location(train_data, test_data, graph, flat_graph):
                 print("-------------------------------")
                 print("Test Data index: ", datum[0])
                 print("Graph index 0: ", graph[index][0])
-                print("Train Data index: ", train_index)
+                print("Train index: ", train_index)
                 print("Graph index 1: ", graph[index][1])
+                print("Train Data Index: ", train_data[train_index][0])
+                print("Friend Latitude: ", train_data[train_index][4])
+                print("Friend Longitude: ", train_data[train_index][5])
                 print("-------------------------------")
-                """
+                # """
                 if train_index < len(train_data) and train_data[train_index][0] == graph[index][1]:
                     # print(" adding new data")
                     sum_latitude += train_data[train_index][4]
@@ -65,6 +66,8 @@ def generate_average_location(train_data, test_data, graph, flat_graph):
             avg_latitude = sum_latitude / n
             avg_longitude = sum_longitude / n
         to_return.append(np.append(datum, [avg_latitude, avg_longitude]))
+    # print("To Return: ")
+    # print(to_return)
     return np.array(to_return)
     pass
 
@@ -129,21 +132,99 @@ if __name__ == "__main__":
 
     flat_graph = graph_data[:, 0].flatten()
     X_tr = generate_average_location(X_tr, X_tr, graph_data, flat_graph)
+    X_te = generate_average_location(X_tr, X_te, graph_data, flat_graph)
+    X_tr = X_tr[:, [1, 2, 3, 7, 8]]
     print("New Training Data: ")
     print(X_tr)
-    X_te = generate_average_location(X_tr, X_te, graph_data, flat_graph)
+    X_te = X_te[:, [1, 2, 3, 7, 8]]
     print("New Testing Data: ")
     print(X_te)
 
+    #####################################
+    # Split into friends and no friends #
+    #####################################
+    X_tr_friends = []
+    X_tr_nofriends = []
+    X_te_friends = []
+    X_te_nofriends = []
+
+    y_tr_lat_friends = []
+    y_tr_lat_nofriends = []
+    y_tr_lon_friends = []
+    y_tr_lon_nofriends = []
+
+    y_te_lat_friends = []
+    y_te_lat_nofriends = []
+    y_te_lon_friends = []
+    y_te_lon_nofriends = []
+
+    y_te = train_data[:25000, [4, 5]]
+
+    for index in range(0, len(X_tr)):
+        row = X_tr[index]
+        y_row = y_tr[index]
+        if row[3] == 0 and row[4] == 0:
+            X_tr_nofriends.append(row)
+            y_tr_lat_nofriends.append(y_row[0])
+            y_tr_lon_nofriends.append(y_row[1])
+        else:
+            X_tr_friends.append(row)
+            y_tr_lat_friends.append(y_row[0])
+            y_tr_lon_friends.append(y_row[1])
+    for index in range(0, len(X_te)):
+        row = X_te[index]
+        y_row = y_te[index]
+        # print("X_te row: ", row)
+        if row[3] == 0 and row[4] == 0:
+            X_te_nofriends.append(row)
+            y_te_lat_nofriends.append(y_row[0])
+            y_te_lon_nofriends.append(y_row[1])
+        else:
+            # print("We have friends!")
+            X_te_friends.append(row)
+            y_te_lat_friends.append(y_row[0])
+            y_te_lon_friends.append(y_row[1])
+
+    X_tr_friends = np.array(X_tr_friends)
+    X_tr_nofriends = np.array(X_tr_nofriends)
+    X_te_friends = np.array(X_te_friends)
+    X_te_nofriends = np.array(X_te_nofriends)
+
+    y_tr_lat_friends = np.array(y_tr_lat_friends)
+    y_tr_lat_nofriends = np.array(y_tr_lat_nofriends)
+    y_tr_lon_friends = np.array(y_tr_lon_friends)
+    y_tr_lon_nofriends = np.array(y_tr_lon_nofriends)
+
+    y_te_lat_friends = np.array(y_te_lat_friends)
+    y_te_lat_nofriends = np.array(y_te_lat_nofriends)
+    y_te_lon_friends = np.array(y_te_lon_friends)
+    y_te_lon_nofriends = np.array(y_te_lon_nofriends)
+    print("There are " + str(len(X_tr_friends)) + " training points with friends")
+    print("There are " + str(len(X_tr_nofriends)) + " training points with no friends")
+    print("There are " + str(len(X_te_friends)) + " testing points with friends")
+    print("There are " + str(len(X_te_nofriends)) + " testing points with no friends")
+
+    """
     scaler = MinMaxScaler()
     scaler.fit(X_tr)
     scaled_X_tr = scaler.transform(X_tr)
     scaler.fit(X_te)
     scaled_X_te = scaler.transform(X_te)
+    """
+    scaler = MinMaxScaler()
+    scaler.fit(X_tr_friends)
+    scaled_X_tr_friends = scaler.transform(X_tr_friends)
+    scaler.fit(X_tr_nofriends)
+    scaled_X_tr_nofriends = scaler.transform(X_tr_nofriends)
 
-    y_te = train_data[:25000, [4, 5]]
-    y_lat_te = train_data[:25000, 4]
-    y_lon_te = train_data[:25000, 5]
+    scaler.fit(X_te_friends)
+    scaled_X_te_friends = scaler.transform(X_te_friends)
+    scaler.fit(X_tr_nofriends)
+    scaled_X_te_nofriends = scaler.transform(X_te_nofriends)
+
+    # y_te = train_data[:25000, [4, 5]]
+    # y_lat_te = train_data[:25000, 4]
+    # y_lon_te = train_data[:25000, 5]
     # """
 
     """
@@ -178,7 +259,7 @@ if __name__ == "__main__":
     clf = SVR(gamma=svr_gamma, kernel=svr_kernel, C=10)
     print("SVR with " + svr_kernel + " kernel and " + svr_gamma + " gamma")
     """
-    # """
+    """
     # clf.fit(X_tr, y_tr)
     clf.fit(scaled_X_tr, y_lat_tr)
     lat_preds = clf.predict(scaled_X_te)
@@ -189,9 +270,21 @@ if __name__ == "__main__":
     lon_preds = clf.predict(scaled_X_te)
     print("Longitude best regressor: ")
     print(clf.cv_results_)
+    """
 
+    clf.fit(scaled_X_tr_friends, y_tr_lat_friends)
+    lat_preds = clf.predict(scaled_X_te_friends)
+    print("Latitude best regressor: ")
+    print(clf.cv_results_)
+
+    clf.fit(scaled_X_tr_friends, y_tr_lon_friends)
+    lon_preds = clf.predict(scaled_X_te_friends)
+    print("Longitude best regressor: ")
+    print(clf.cv_results_)
+    # """
     predictions = np.column_stack((lat_preds, lon_preds))
-    error = RMSE(y_te, predictions)
+    y_te_friends = np.column_stack((y_te_lat_friends, y_te_lon_friends))
+    error = RMSE(y_te_friends, predictions)
 
     ##############
     # Gridsearch #
@@ -199,7 +292,7 @@ if __name__ == "__main__":
 
     print("RMSE Error: ", error)
 
-    mseError = sqrt(mean_squared_error(y_te, predictions))
+    mseError = sqrt(mean_squared_error(y_te_friends, predictions))
     print("Scikit Learn Error: ", mseError)
     # """
     end_time = datetime.datetime.now()
